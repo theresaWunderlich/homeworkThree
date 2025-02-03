249399ea7719cc246c1484edfa3de8ce0697b59c
Theresa Wunderlich <99081720+theresaWunderlich@users.noreply.github.com>
2025-02-02 18:03:17 -0800

    part seven

---
+++ hw3/tests/test.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const $ = require('jquery');
+issuestest('test selectEvent', () => {     
+ 
+// Read index.html into a stringconst
+html = fs.readFileSync('public/index.html', 'utf8');     
+expect(html).toEqual(expect.anything()); // Check that HTML is not null
+// Load the HTML into Jest's mock DOM
+document.body.innerHTML = html;    
+ // Ensure DOM is fully loaded before querying   
+ $(function() {         
+expect($('h1').html()).toBe("Cheesecake Order Form"); 
+}); 
+});
